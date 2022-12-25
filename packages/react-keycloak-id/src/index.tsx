15bcdbde9fd1622781bd686ccf712f0832a56750
Ugi Ispoyo Widodo <ugi.widodo@dataon.com>
2022-12-25 21:30:19 +0700

    fix: 🐛 fix-publish
    bug fix commit

--- packages/react-keycloak-id/src/index.tsx
+++ packages/react-keycloak-id/src/index.tsx
@@ -62,7 +62,7 @@ export const ReactKeycloackIdProvider = ({ init, children }: TReactKeycloackProv
                 )
             ) : (
                 <>
-                    Loading...
+                    Initialization keycloak...
                 </>
             )}
         </>
